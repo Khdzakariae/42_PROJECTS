bc516240440fc09683ae44ecdd1d11dba8b26f23
Zakariae El-Khadir <zel-khad@c3r8p7.1337.ma>
2024-02-29 16:15:11 +0100

    linux

---
+++ Minitalk/sever.c
@@ -0,0 +1,25 @@
+#include "minitalk.h"
+
+void	handler(int sig)
+{
+    static int  tmp1 = 0;
+	char tmp;
+	tmp = tmp << 1 | (sig == SIGUSR1);
+	tmp1++;
+	if (tmp1 == 8)
+	{
+		write(1, &tmp, 1);
+		tmp1 = 0;
+	}
+}
+
+int	main(void)
+{
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	printf("THE PID OF THE SERVER is %d\n", getpid());
+	while (1)
+	{
+	}
+	
+}
